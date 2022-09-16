065e37327c74d16ba8ce8805e3d6d00a89bf1920
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2022-09-16 10:02:25 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -8,11 +8,7 @@ import NotificationButton from "../NotificationButton";
 import "./styles.css";
 
 function SalesCard() {
-  useEffect(() => {
-    axios.get(`${BASE_URL}/sales`).then((response) => {
-      setSales(response.data.content);
-    });
-  }, []);
+  
 
   const min = new Date(new Date().setDate(new Date().getDate() - 365));
   const max = new Date();
@@ -21,6 +17,16 @@ function SalesCard() {
   const [maxDate, setMaxDate] = useState(max);
 
   const [sales, setSales] = useState<Sale[]>([]);
+
+  useEffect(() => {
+    
+    const dmin = minDate.toISOString().slice(0, 10);
+    const dmax = maxDate.toISOString().slice(0, 10);
+
+    axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then((response) => {
+      setSales(response.data.content);
+    });
+  }, [minDate, maxDate]);
 
   return (
     <div className="dsmeta-card">
@@ -37,7 +43,7 @@ function SalesCard() {
         <div className="dsmeta-form-control-container">
           <DatePicker
             selected={maxDate}
-            onChange={(date: Date) => setMaxDate(max)}
+            onChange={(date: Date) => setMaxDate(date)}
             className="dsmeta-form-control"
             dateFormat="dd/MM/yyyy"
           />
